07788a7a380229b177d281b4515af80438b3705c
Agnes <almoha@gmail.com>
2023-02-13 12:30:45 +0100

    logo accueil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,11 @@ import Navbar from "../components/Navbar"
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-yellow-50'>
+    <div className='w-full h-screen bg-yellow-50 flex flex-col items-center '>
         <Navbar />
+        <main className='flex flex-col justify-center w-10/12 items-center'>
+          <img src="https://firebasestorage.googleapis.com/v0/b/francine-94efd.appspot.com/o/logo%2FlogoText.png?alt=media&token=4d2444ad-404f-4d77-9cc6-4a0751bcb924" alt="" />
+        </main>
     </div>
   )
 }
